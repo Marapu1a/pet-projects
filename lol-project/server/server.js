0946ce555990bea432312295a4942325f43ef040
Marapu1a <137860083+Marapu1a@users.noreply.github.com>
2024-10-15 14:36:13 +0300

    Update server.js

--- lol-project/server/server.js
+++ lol-project/server/server.js
@@ -6,7 +6,7 @@ const port = 3001;
 
 app.get('/riot/account/v1/accounts/by-riot-id/:gameName/:tagLine', async (req, res) => {
   const { gameName, tagLine } = req.params;
-  const apiKey = 'RGAPI-54e37433-13fc-40ec-be31-44716b6f6ec2'; // Замени на свой Riot API ключ
+  const apiKey = 'ВСТАВЬ СЮДА КЛЮЧ';
 
   try {
     const response = await axios.get(`https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`, {
